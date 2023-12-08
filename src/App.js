a4549d4784c236d1bae37baed5aab2203a73f713
adityamullick1 <adityamullick98@gmail.com>
2023-12-07 20:24:58 -0800

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,73 +1,105 @@
-import './App.css';
-import {useState} from "react";
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
+import styled from 'styled-components';
 
-const fun = () => {
+const LandingPage = () => {
+    const formRef = useRef();
 
-}
-function App() {
-    const [newItem, setNewItem] = useState("")
-    const [todo, setTodos] = useState([])
-    const [text, setText] = useState("")
-    const [val, setVal] = useState(0)
+    const sendEmail = (event) => {
+        event.preventDefault();
 
+        emailjs
+            .sendForm('adityaService', 'template_b92o3ed', formRef.current, 'kDv14N1AfXfoYqOKY')
+            .then((result) => {
+                console.log(result.text);
+                // Display success message
+            }, (error) => {
+                console.log(error.text);
+                // Display error message
+            });
+    };
 
+    return (<Wrapper>
+        <Title>Welcome to Reflectify!</Title>
+        <Message>Leave some feedback on what you want.</Message>
+        <Form ref={formRef} onSubmit={sendEmail}>
+            <Label>Name</Label>
+            <Input
+                type="text"
+                name="to_name"/>
+            <Label>Email</Label>
+            <Input
+                type="email"
+                name="from_name"/>
+            <Label>Message</Label>
+            <Textarea
+                name="message"/>
+            <Button type="submit">Send</Button>
+        </Form>
+    </Wrapper>);
+};
 
+const Wrapper = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
 
+const Title = styled.h1`
+  font-size: 3rem;
+  color: #333;
+  margin: 0;
+  text-align: center;
+`;
 
-    function handleClick() {
-        console.log("Clicked delete")
-        // send an RPC to a server
-        const xhr = new XMLHttpRequest()
-        xhr.addEventListener("progress", (event) => {
-            setVal(event.loaded)
-            console.log("loading")
-        })
-        xhr.open('GET', "https://run.mocky.io/v3/0c46397c-be0b-4adb-a5bd-781034fb81fc")
-        xhr.onload = function() {
-            if (xhr.status == 200) {
-                setText(xhr.responseText)
-            }
-        }
-        xhr.send()
+const Message = styled.p`
+  font-size: 1.5rem;
+  color: #666;
+  margin: 10px 0;
+  text-align: center;
+`;
 
-    }
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  padding: 20px;
+  border-radius: 5px;
+  background-color: #eee;
+`;
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        setTodos(curr => {
-            return [
-            ...curr,
-            {id: crypto.randomUUID(), title: newItem, completed: false}
-        ]})
-    }
+const Label = styled.label`
+  font-size: 1.2rem;
+  margin-bottom: 5px;
+`;
 
+const Input = styled.input`
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  margin-bottom: 10px;
+  font-size: 1.2rem;
+`;
 
-    console.log(todo)
+const Textarea = styled.textarea`
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  margin-bottom: 10px;
+  font-size: 1.2rem;
+  height: 100px;
+`;
 
-  return <> <form onSubmit={handleSubmit} className="new-item-form">
-      <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input value={newItem} onChange={e=>setNewItem(e.target.value)} type="text" id="item" />
-      </div>
-  </form>
-    <h1 className="header">Todo list</h1>
-    <u1 className="list">
-        {todo.map(tod => {
-            return <li key={todo.id}>
-                <label>
-                    <input type="checkbox" checked={tod.completed} />
-                    {tod.id}
-                </label>
-                <button onClick={handleClick} className="btn btn-danger">{"delete"}</button>
-            </li>
-        })}
-    </u1>
-      <h1>{text}</h1>
-      <h1>{val}</h1>
+const Button = styled.button`
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: 1px solid #333;
+  background-color: #333;
+  color: #fff;
+  cursor: pointer;
+`;
 
-  </>
-
-}
-
-
-export default App;
+export default LandingPage;
