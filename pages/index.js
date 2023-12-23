4e46b71d19cb34efeccc78fe4d49389bd29c721b
adityamullick1 <adityamullick98@gmail.com>
2023-12-23 13:02:31 -0800

    integrate nextjs tutorial

---
+++ pages/index.js
@@ -0,0 +1,94 @@
+import React, {useRef, useState} from 'react';
+import emailjs from '@emailjs/browser';
+import Link from 'next/link';
+import axios from "axios";
+import ErrorDialog, {Button, Form, Input, Label, Message, Textarea, Title, Wrapper} from "./components.tsx";
+
+
+const promptTitle = "Send a prompt";
+const openAiErrorString = "OpenAI being a bitch";
+
+const Home = () => {
+    const formRef = useRef();
+    const promptRef = useRef()
+    const [showError, setShowError] = useState(false)
+
+    const sendEmail = (event) => {
+        event.preventDefault();
+        emailjs
+            .sendForm('adityaService', 'template_b92o3ed', formRef.current, 'kDv14N1AfXfoYqOKY')
+            .then((result) => {
+                console.log(result.text);
+                // Display success message
+            }, (error) => {
+                console.log(error.text);
+                // Display error message
+            });
+    };
+
+    const sendPrompt = (event) => {
+        event.preventDefault();
+        console.log("calling gpt");
+        console.log(promptRef.current.value);
+
+        callChatGptServer(promptRef.current.value).then((result) => {
+            console.log(result.value);
+        }).catch((error) => {
+            console.log("Showing error");
+            setShowError(true)
+
+        })
+    };
+
+    return (<Wrapper>
+        <Link href="/about">
+            Go to the about page
+        </Link>
+        <Title>Welcome to Reflectify!</Title>
+        <Message>Leave some feedback on what you want.</Message>
+        <Form ref={formRef} onSubmit={sendEmail}>
+            <Label>Name</Label>
+            <Input
+                type="text"
+                name="to_name"/>
+            <Label>Email</Label>
+            <Input
+                type="email"
+                name="from_name"/>
+            <Label>Message</Label>
+            <Textarea
+                name="message"/>
+            <Button type="submit">Send</Button>
+        </Form>
+        <Form onSubmit={sendPrompt}>
+            <Label>Talk to me!</Label>
+            <Input
+                type="text" ref={promptRef}/>
+            <Button type="submit">Send</Button>
+        </Form>
+        <ErrorDialog show={showError} onClose={() => setShowError(false)}
+                     title={promptTitle}
+                     message={openAiErrorString}/>
+    </Wrapper>);
+};
+
+const callChatGptServer = async (message) => {
+    const url = 'http://localhost:3001/chat'; // API endpoint
+    console.log("request body" + message.toString());
+    const apiRequestBody = {
+        prompt: message
+    };
+    try {
+        const response = await axios.post(
+            url,
+            apiRequestBody
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error calling local server:', error);
+        throw error;
+    }
+}
+
+
+export default Home;
---
+++ pages/server.ts
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/chat', async (req, res, retries= 3, delay=1000) => {
+    try {
+        const userRequestPrompt = req.body.prompt;
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                { role: "system", content: userRequestPrompt },
+            ],
+        }, {
+            headers: {
+                'Authorization': `Bearer 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`, // OPENAI API KEY: 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
+                'Content-Type': 'application/json'
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error calling OpenAI API:', error);
+        res.status(500).send('Error processing your request');
+    }
+});
+
+const port = 3001;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
